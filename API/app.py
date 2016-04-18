b62833554e07c24d1decc22249f5d3fcea7b6fc6
lnukala <leelakrishnanukala27@gmail.com>
2016-04-18 15:56:50 -0700

    Few APIs added

--- API/app.py
+++ API/app.py
@@ -1,56 +1,58 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import  Resource, Api
 import requests
 import json
-
 
 app = Flask(__name__)
 api = Api(app)
 
 #Run the within distance query and return the values in that circle
 class WithinDistance (Resource):
-    def get(self, lat, long, distance):
+    def get(self):
+        data = json.loads(request.get_data())
         url = 'http://localhost:7474/db/data/ext/SpatialPlugin/graphdb/findGeometriesWithinDistance'
-        payload = {"layer" : 'geom',  'pointX':lat, 'pointY': long, "distanceInKm" : distance}
+        payload = {"layer" : data.get('date'),  'pointX': data.get('lon'), 'pointY': data.get('lat'),
+                   "distanceInKm" : data.get('distance')}
+        print(payload)
         r = requests.post(url, data=json.dumps(payload), auth=('neo4j', '18843'))
         if r.status_code != 200:
-            return r._content, r.status_code
+            return "Error!", r.status_code
         content = json.loads(r.content)
-        sum = 0
-        count = 0
+        data = []
         for object in content :
             if 'value' in object['data']:
-                value = object['data'].get('value')
-                print(value)
-                sum += float(value)
-                count += 1
-        print ("final value of the region is %f", sum/count) #Take average
-        return json.dumps({"value" : value})
+                info = {
+                    'value' : object['data'].get('value'),
+                    'lat' : object['data'].get('lat'),
+                    'lon' : object['data'].get('lon')
+                }
+                data.append(info)
+        return json.dumps(data)
 
+#For bounding box queries
 class BBox (Resource):
-    def get(self, minx , miny , maxx, maxy):
+    def get(self):
+        data = json.loads(request.get_data())
         url = 'http://localhost:7474/db/data/ext/SpatialPlugin/graphdb/findGeometriesInBBox'
-        payload = {"layer" : 'geom',  'miny':miny, 'maxy': maxy, 'minx':minx, 'maxx':maxx}
+        payload = {"layer" : data.get('date'),  'miny':data.get('miny'), 'maxy': data.get('maxy'),
+                  'minx': data.get('minx'), 'maxx':data.get('maxx')}
         r = requests.post(url, data=json.dumps(payload), auth=('neo4j', '18843'))
         if r.status_code != 200:
-            return r._content, r.status_code
+            return "Error!", r.status_code
         content = json.loads(r.content)
-        sum = 0
-        count = 0
+        data = []
         for object in content :
             if 'value' in object['data']:
-                value = object['data'].get('value')
-                print(value)
-                sum += float(value)
-                count += 1
-        print ("final value of the region is %f", sum/count) #Take average
-        return json.dumps({"value" : value})
+                info = {
+                    'value' : object['data'].get('value'),
+                    'lat' : object['data'].get('lat'),
+                    'lon' : object['data'].get('lon')
+                }
+                data.append(info)
+        return json.dumps(data)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+api.add_resource(WithinDistance, '/withindist/')
+api.add_resource(BBox, '/boundingbox')
 
 if __name__ == '__main__':
     app.run()
