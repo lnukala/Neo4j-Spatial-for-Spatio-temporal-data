7442d79626a52f071152a2103d7234d9b68e68a6
lnukala <leelakrishnanukala27@gmail.com>
2016-04-18 17:04:40 -0700

    added date based api

--- API/app.py
+++ API/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import  Resource, Api
 import requests
 import json
+from datetime import timedelta, datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,7 +14,6 @@ class WithinDistance (Resource):
         url = 'http://localhost:7474/db/data/ext/SpatialPlugin/graphdb/findGeometriesWithinDistance'
         payload = {"layer" : data.get('date'),  'pointX': data.get('lon'), 'pointY': data.get('lat'),
                    "distanceInKm" : data.get('distance')}
-        print(payload)
         r = requests.post(url, data=json.dumps(payload), auth=('neo4j', '18843'))
         if r.status_code != 200:
             return "Error!", r.status_code
@@ -50,6 +50,35 @@ class BBox (Resource):
                 }
                 data.append(info)
         return json.dumps(data)
+
+class GetTile(Resource):
+    def get(self):
+        data = json.loads(request.get_data())
+        url = 'http://localhost:7474/db/data/ext/SpatialPlugin/graphdb/findGeometriesWithinDistance'
+        date_list = [data.get('end_date') - datetime.timedelta(days=x)
+                     for x in range(0, (data.get('end_date') - data.get('start_date')).days())]
+        result = []
+        for date in date_list:
+            payload = {"layer" : date,  'pointX': data.get('lon'), 'pointY': data.get('lat'),
+                   "distanceInKm" : 0.25}
+            r = requests.post(url, data=json.dumps(payload), auth=('neo4j', '18843'))
+            if r.status_code != 200:
+                return "Error!", r.status_code
+            content = json.loads(r.content)
+            values = []
+            for object in content :
+                if 'value' in object['data']:
+                    info = {
+                        'date' : date,
+                        'value' : object['data'].get('value'),
+                        'lat' : object['data'].get('lat'),
+                        'lon' : object['data'].get('lon')
+                    }
+                values.append(info)
+            result.append(values)
+        return json.dumps(result)
+
+
 
 api.add_resource(WithinDistance, '/withindist/')
 api.add_resource(BBox, '/boundingbox')
